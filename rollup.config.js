b0efa5a02f2b4d660f962feb051f3c6d25e58cba
Richard Williams <richard.williams@manchester.ac.uk>
2019-10-21 22:10:13 +0100

    Add new files

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,14 @@ import copy from 'rollup-plugin-copy'
 export default {
   input: 'src/main.js',
   output: {
-    file: 'dist/bundle.js',
+    file: './bundle.js',
     format: 'iife'
   },
   plugins: [
     copy({
       targets: [
-        { src: 'src/index.html', dest: 'dist' },
-        { src: 'src/socket.io.js', dest: 'dist' },
+        { src: 'src/index.html', dest: '.' },
+        { src: 'src/socket.io.js', dest: '.' },
       ]
     }),
   ],
