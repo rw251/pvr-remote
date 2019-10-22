51b101188722a2e7e43d8407011fe11a163fad8b
Richard Williams <richard.williams@manchester.ac.uk>
2019-10-22 22:25:55 +0100

    Updated

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,13 @@ import copy from 'rollup-plugin-copy'
 export default {
   input: 'src/main.js',
   output: {
-    file: './bundle.js',
+    file: './dist/bundle.js',
     format: 'iife'
   },
   plugins: [
     copy({
       targets: [
-        { src: 'src/index.html', dest: '.' },
-        { src: 'src/socket.io.js', dest: '.' },
+        { src: 'src/index.html', dest: 'dist/' },
       ]
     }),
   ],
--- src/main.js
+++ src/main.js
@@ -1,42 +1,46 @@
 const port = 8812;
 const address = '192.168.1.11';
-let socket = io(`http://${address}:${port}`);
+let socket = new WebSocket(`ws://${address}:${port}`);
+
+const onOpen = () => {
+	
+	// Listen for messages
+	socket.onmessage = () => {
+    showRemote();
+	};
+	
+	// Listen for socket closes
+	socket.onclose = (event) => {
+		console.log('Client notified socket has closed',event);
+	};
+};
+
+const onError = () => {
+  socket.close();
+  document.getElementById('newSocket').style.display = 'block';
+  document.getElementById('newIP').onclick = tryNewIP;
+};
 
 const showRemote = () => {
   const remote = document.getElementById('remote');
   remote.style.display = 'block';
-  remote.addEventListener('click', (e) => {
+  remote.onclick = (e) => {
     if(e.target.dataset.button) {
       console.log(e.target.dataset.button);
-      socket.emit('button', {value: e.target.dataset.button})
+      socket.send(JSON.stringify( { type: 'button', value: e.target.dataset.button}));
     }
-  });
+  };
 };
 
 const tryNewIP = () => {
   document.getElementById('newSocket').style.display = 'none';
   document.getElementById('newIP').removeEventListener('click', tryNewIP);
   const newIPAddress = document.getElementById('newIPAddress').value;
-  socket = io(`http://${newIPAddress}:${port}`);
-  socket.on("welcome", (message) => {
-    console.log(message)
-  })
+  socket = new WebSocket(`ws://${newIPAddress}:${port}`);
+  socket.onopen = onOpen;	
+  socket.onerror = onError;
   showRemote();
 };
 
-socket.on("welcome", (message) => {
-  console.log(message)
-  showRemote();
-})
-
-socket.on("connect", () => {
-  console.log('connected')
-})
-socket.on("connect_timeout", () => {
-  console.log('connect_timeout')
-})
-socket.on("connect_error", () => {
-  socket.close();
-  document.getElementById('newSocket').style.display = 'block';
-  document.getElementById('newIP').addEventListener('click', tryNewIP);
-})
+socket.onopen = onOpen;	
+socket.onerror = onError;
